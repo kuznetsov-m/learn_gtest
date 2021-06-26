7ba048d9f3220bc055ecf8a8501d52aa8581b510
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-26 20:30:00 +0300

    test: add example_lib_tests.cpp

---
+++ test/example_lib_tests.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include <example.hpp>
+
+#include <math.h>
+
+// for windows build
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+TEST(test, circle_square) {
+    double radius = 10.;
+    double expectedSquare = radius * M_PI;
+    EXPECT_DOUBLE_EQ(
+        expectedSquare,
+        circleSquare(radius)
+    );
+}
