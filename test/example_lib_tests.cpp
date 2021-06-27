b3480296848fa20a241362fc402026662c212967
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-27 19:30:03 +0300

    test: add tests for process function

--- test/example_lib_tests.cpp
+++ test/example_lib_tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <example.hpp>
+#include <functions.h>
 
 #include <math.h>
 
@@ -27,4 +28,77 @@ TEST(example_lib, triangleSquare) {
     double h = 5.;
     double expectedSquare = a * h / 2.;
     EXPECT_DOUBLE_EQ(expectedSquare, triangleSquare(a, h));
+}
+
+// shape=circle
+TEST(example_lib, process_circle_1) {
+    const int argc = 3;
+    const char *args[argc] = { "app_program", "--shape=circle", "10"};
+
+    EXPECT_EQ(process(argc, args), true);
+}
+
+TEST(example_lib, process_circle_2) {
+    const int argc = 2;
+    const char *args[argc] = { "app_program", "--shape=circle"};
+
+    EXPECT_EQ(process(argc, args), false);
+}
+
+// shape=rectangle
+TEST(example_lib, process_rectangle_1) {
+    const int argc = 4;
+    const char *args[argc] = { "app_program", "--shape=rectangle", "10", "10"};
+
+    EXPECT_EQ(process(argc, args), true);
+}
+
+TEST(example_lib, process_rectangle_2) {
+    const int argc = 3;
+    const char *args[argc] = { "app_program", "--shape=rectangle", "10"};
+
+    EXPECT_EQ(process(argc, args), false);
+}
+
+TEST(example_lib, process_rectangle_3) {
+    const int argc = 5;
+    const char *args[argc] = { "app_program", "--shape=rectangle", "10", "11", "10"};
+
+    EXPECT_EQ(process(argc, args), true);
+}
+
+TEST(example_lib, process_rectangle_4) {
+    const int argc = 5;
+    const char *args[argc] = { "app_program", "--shape=rectangle", "10", "11", "any_arg"};
+
+    EXPECT_EQ(process(argc, args), false);
+}
+
+// shape=triangle
+TEST(example_lib, process_triangle_1) {
+    const int argc = 4;
+    const char *args[argc] = { "app_program", "--shape=triangle", "10", "10"};
+
+    EXPECT_EQ(process(argc, args), true);
+}
+
+TEST(example_lib, process_triangle_2) {
+    const int argc = 3;
+    const char *args[argc] = { "app_program", "--shape=triangle", "10"};
+
+    EXPECT_EQ(process(argc, args), false);
+}
+
+TEST(example_lib, process_triangle_3) {
+    const int argc = 5;
+    const char *args[argc] = { "app_program", "--shape=triangle", "10", "11", "10"};
+
+    EXPECT_EQ(process(argc, args), true);
+}
+
+TEST(example_lib, process_triangle_4) {
+    const int argc = 5;
+    const char *args[argc] = { "app_program", "--shape=triangle", "10", "11", "any_arg"};
+
+    EXPECT_EQ(process(argc, args), false);
 }
